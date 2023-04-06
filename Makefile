1f5e492420c41854f1a53151fcd18653a6b32243
Luis Colorado <luis.colorado@spindrive.fi>
2023-04-06 23:24:49 +0300

    Initial revision (tested)
    Signed-off-by: Luis Colorado <luis.colorado@spindrive.fi>

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Makefile -- build script for pick(1)
+# Author: Luis Colorado <luiscoloradourcola@gmail.com>
+# Date: Thu Apr  6 01:57:46 PM EEST 2023
+# Copyright: (c) 2023 Luis Colorado.  All rights reserved.
+# License: BSD
+
+targets            = pick pick.1.gz
+toclean            = $(targets)
+
+prefix            ?= /usr/local
+exec_prefix       ?= $(prefix)
+bindir            ?= $(exec_prefix)/bin
+datarootdir       ?= $(prefix)/share
+mandir            ?= $(datarootdir)/man
+man1dir           ?= $(mandir)/man1
+
+XMOD              ?= -m 0755     # binary files
+MMOD              ?= -m 0644     # manual page files
+DMOD              ?= -m 0755     # directories modes
+
+GROFF             ?= groff
+RM	              ?= -rm -f
+INSTALL           ?= install
+IFLAGS            ?= -o root -g root
+GROFF             ?= groff
+
+pick_deps = 
+pick_objs = pick.o
+pick_ldlf =
+pick_libs =
+toclean += $(pick_objs)
+
+all: $(targets)
+clean:
+	$(RM) $(toclean)
+install: $(targets)
+	$(INSTALL) $(IFLAGS) $(DMOD) -d $(bindir)
+	$(INSTALL) $(IFLAGS) $(DMOD) -d $(man1dir)
+	$(INSTALL) $(IFLAGS) $(XMOD) pick $(bindir)
+	$(INSTALL) $(IFLAGS) $(XMOD) pick.1.gz $(man1dir)
+
+pick: $(pick_deps) $(pick_objs)
+	$(CC) -o $@ $(LDFLAGS) $($@_ldfl) $($@_objs) $($@_libs) $(LIBS)
+
+pick.1.pdf: pick.1
+	soelim $^ | $(GROFF) $(GROFFFLAGS) -Tpdf -mandoc >$@
+
+pick.1.gz: pick.1
+	gzip < $? > $@
---
+++ pick.c
@@ -0,0 +1,74 @@
+/* pick.c -- program to make a selection for the shell.
+ * (Idea based on pick program from The UNIX programming
+ * environment, Kernighan&Pike)
+ * Author: Luis Colorado <luiscoloradourcola@gmail.com>
+ * Date: Thu Apr  6 11:09:55 AM EEST 2023
+ * Copyright: (c) 2023 Luis Colorado.  All rights reserved.
+ * License: BSD.
+ */
+
+#include <ctype.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ADD          (0)
+#define SUB          ('y' ^ 'n')
+
+char **pick(
+		char **argv_r,
+		int    defop,
+		int    add_sub)
+{
+    char **saved  = argv_r,
+         **argv_l = argv_r;
+
+    for (; *argv_r; argv_r++) {
+        char line[64],
+			*aux;
+        do {
+            fprintf(stderr, "%s? ", *argv_r);
+            if (!fgets(line, sizeof line, stdin))
+                return NULL;
+            aux = strtok(line, " \t\n");
+        } while (aux && (aux = strchr("yYnN", *aux)) == NULL);
+
+		int op = (aux ? tolower(*aux) : defop) ^ add_sub;
+				
+		if (op == 'y') { /* add */
+			if (argv_l != argv_r)
+				*argv_l = *argv_r;
+			argv_l++;
+        }
+    }
+    /* copy last null */
+    if (argv_l != argv_r) {
+        *argv_l = *argv_r;
+    }
+    return saved;
+}
+
+int main(int argc, char **argv)
+{
+	int opt,
+		def     = 'y',
+		add_sub = 0;
+
+	while ((opt = getopt(argc, argv, "asny")) != EOF) {
+		switch (opt) {
+		case 'a': add_sub = ADD; break;
+		case 's': add_sub = SUB; break;
+		case 'n': def     = 'n'; break; /* 'default' is no */
+		case 'Y': def     = 'y'; break; /* 'default' is yes */
+		}
+	}
+	argc -= optind; argv += optind;
+
+    argv = pick(argv, def, add_sub);
+
+    for (;*argv; argv++)
+        printf("%s\n", *argv);
+
+    return 0;
+}
